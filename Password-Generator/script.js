6dc1b8783d4a15f4f9d1c4554faa7364a287d79b
KAMRUDDIN KHAN <kamruddinkhandgp@gmail.com>
2023-05-28 22:24:07 +0530

    App-Functions Added

--- Password-Generator/script.js
+++ Password-Generator/script.js
@@ -17,6 +17,7 @@ let pass="";
 let passwordLength=10;
 let checkboxStatus=1;
 let strengthOfPassword;
+const symbols="+-*/(){}[]`<>:;'!@#$"
 
  
 // This function sets the password length accordin to slider input
@@ -33,11 +34,46 @@ function getRandomInt(min,max){
     return Math.floor((Math.random() * (max-min))+min); 
 }
 
-function genearteRandomNo(){
+function generateRandomNo(){
     return getRandomInt(0,9);
 }
 
 function getLowerCase(){
     return String.fromCharCode(getRandomInt(97,123));
 }
-// 
+
+function getUpperCase(){
+    return String.fromCharCode(getRandomInt(65,91));
+}
+
+function generateSymbol(){
+    return symbols.charAt[getRandomInt(0,symbols.length)];
+}
+
+function calcPasswordStrength(){
+    let hasUpper=false;
+    let hasLower=false;
+    let hasNum=false;
+    let hasSym=false;
+
+    if(upperCaseChck.checked) hasUpper=true;
+    if(lowerCaseChck.checked) hasLower=true;
+    if(numbersChck.checked) hasNum=true;
+    if(symbolsChck.checked) hasSym=true;
+    
+
+    if(hasUpper && hasLower && hasNum && hasSym && passwordLength>=8)
+    {
+        setStrengthIndicator("strong");
+    }
+
+    else if(hasUpper && hasLower && (hasNum || hasSym) && passwordLength>=6)
+    {
+        setStrengthIndicator("medium");
+    }
+
+    else
+    {
+        setStrengthIndicator("hard");
+    }
+}
